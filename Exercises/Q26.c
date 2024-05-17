708c2ff6c9aa177340c16cad66ba55c674f1b1bb
Rael <santosrafaelpro@gmail.com>
2024-05-17 11:41:14 -0300

    Q26 updated

---
+++ Exercises/Q26.c
@@ -0,0 +1,15 @@
+
+//* tabuada do 7 ate 10
+
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 7; i <= 70; i += 7)
+    {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
---
+++ Exercises/Q27.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    //* vamos ve os quadrados dos numeros entre 15 e 30
+
+    for (int i = 16; i < 30; i=i+1){
+        
+        printf("%d^2 = %d\n", i, i*i); //* tbm é possivel printar assim: printf("%d ", i*i);
+    }
+
+    return 0;
+}
