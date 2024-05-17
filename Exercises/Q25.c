1ef5df6d5035fd6ff30619f2a7bd3bd5d3ef8510
Rael <santosrafaelpro@gmail.com>
2024-05-17 11:28:29 -0300

    Update Q25.c

--- Exercises/Q25.c
+++ Exercises/Q25.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
 
-    
+    //* Vou mostrar os numeros pares de 14 a 30:
 
+    for (int i = 14; i <= 30; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d", i);
+        }
+    }
 
     return 0;
 }
