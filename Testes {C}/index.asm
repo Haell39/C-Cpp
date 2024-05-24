5c48d8741c7b6d4561e1736f4fd061a46c1aacee
Rael <santosrafaelpro@gmail.com>
2024-05-24 12:19:55 -0300

    updt asm

--- Testes {C}/index.asm
+++ Testes {C}/index.asm
@@ -1,15 +1,63 @@
-DATA SEGMENT USE16
-MESG DB 'Hello word','$'
-DATA ENDS
-CODE SEGMENT USE16
-    ASSUME CS:CODE,DS:DATA
-BEG:
-    MOV AX,DATA
-    MOV DS,AX
-    MOV AH,9
-    MOV DX, OFFSET MESG
-    INT 21H
-    MOV AH,4CH
-    INT 21H;back to dos
-CODE ENDS
-END BEG
+section .data
+    msg1 db 'Digite o primeiro numero: ',0
+    msg2 db 'Digite o segundo numero: ',0
+    msg3 db 'A soma dos numeros eh: ',0
+
+section .bss
+    num1 resd 1
+    num2 resd 1
+
+section .text
+    global _start
+
+_start:
+    ; imprimir msg1
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg1
+    mov edx, 31
+    int 80h
+
+    ; ler num1
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, num1
+    mov edx, 4
+    int 80h
+
+    ; imprimir msg2
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg2
+    mov edx, 28
+    int 80h
+
+    ; ler num2
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, num2
+    mov edx, 4
+    int 80h
+
+    ; somar
+    mov eax, [num1]
+    add eax, [num2]
+
+    ; imprimir msg3
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg3
+    mov edx, 24
+    int 80h
+
+    ; imprimir resultado
+    mov eax, 1
+    mov ebx, 1
+    mov ecx, eax
+    mov edx, 1
+    int 80h
+
+    ; encerrar programa
+    mov eax, 1
+    xor ebx, ebx
+    int 80h
