94ed232b6ee8d2aaf97aa55f86cefc3a8e0aedf7
Rael <santosrafaelpro@gmail.com>
2024-06-07 11:34:10 -0300

    NOTHING

--- Testes {C}/Q6.c
+++ Testes {C}/Q6.c
@@ -13,7 +13,7 @@ int main()
         // 3. `%u` é um especificador de formato que indica que o valor a ser impresso será interpretado como um inteiro sem sinal.
         // 4. `\n` é um caractere de quebra de linha que indica que a linha será finalizada e um nova linha será impressa.
         // Dessa forma, a linha acima está imprimindo o valor de `i` incrementado em 1 e multiplicado por 4, em cada iteração do loop.
-        printf("O valor de i incrementado em 1 e multiplicado por 4 na iteração %d eh: %u\n", i+1, ++i<<2);
+        printf("O valor de i incrementado em 1 e multiplicado por 4 na iteração %d eh: %u\n", i+1, ++i<<3);
     }
     return 0;
 }
