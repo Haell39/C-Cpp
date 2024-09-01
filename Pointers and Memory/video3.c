f9f548975c88c30798853fbce36e2b4b33de5e23
Rael <santosrafaelpro@gmail.com>
2024-08-31 23:50:15 -0300

    updates

--- Pointers and Memory/video3.c
+++ Pointers and Memory/video3.c
@@ -16,6 +16,13 @@ int main()
     int f = 1;
     double g = 3.14;
 
+    char l = 'L';
+    char k[1];
+    char m[10];
+    short n[2];
+    int o[3];
+    double p[4];
+
     printf("%d bytes\n", sizeof(a));
     printf("%d bytes\n", sizeof(b));
     printf("%d bytes\n", sizeof(c));
@@ -35,6 +42,24 @@ int main()
 
     printf("%p\n", &f);
     printf("%p\n", &g);
+
+    printf("%d bytes\n", sizeof(l));
+    printf("%p\n", &l);
+
+    printf("%d bytes\n", sizeof(k));
+    printf("%p\n", &k);
+    printf("%d bytes\n", sizeof(m));
+    printf("%p\n", &m);
+
+    printf("%d bytes\n", sizeof(n));
+    printf("%d bytes\n", sizeof(o));
+    printf("%d bytes\n", sizeof(p));
+
+    printf("%p\n", &n);
+    printf("%p\n", &o);
+    printf("%p\n", &p);
+
+
 
 
 
